d09d8465da3e6350927a2fb3c36ee288035088fc
K750i <munkin80@gmail.com>
2018-12-31 15:04:26 +0800

    Only do parallax when header is visible

--- js/functions.js
+++ js/functions.js
@@ -29,11 +29,13 @@ function debounce(func, wait = 20, immediate = true) {
 function checkScroll() {
   var vScroll = window.pageYOffset;
 
-  logo.style.transform = `translateY(${vScroll / 2}%)`;
-  backBird.style.transform = `translate(-50%, ${vScroll / 4}%)`;
-  foreBird.style.transform = `translate(-50%, -${vScroll / 40}%)`;
-  birdBox.style.backgroundPosition = `center -${(vScroll / 6)}px`;
-
+  if (vScroll <= birdBox.clientHeight) {
+    logo.style.transform = `translateY(${vScroll / 2}%)`;
+    backBird.style.transform = `translate(-50%, ${vScroll / 4}%)`;
+    foreBird.style.transform = `translate(-50%, -${vScroll / 40}%)`;
+    birdBox.style.backgroundPosition = `center -${(vScroll / 6)}px`;
+  }
+  
   if (vScroll > container.offsetTop - (window.innerHeight / 2)) {
     figure.forEach(function(item, index) {
       setTimeout(() => item.classList.add('is-showing'), 100 * index);
